aefdbcf01d214805efb24e54b7e4d86b6c7075f6
26199752+yuudi@users.noreply.github.com <26199752+yuudi@users.noreply.github.com>
2019-10-16 11:44:49 +0800

    complete jjc_consult

--- src/client/jjc_consult.py
+++ src/client/jjc_consult.py
@@ -1,7 +1,5 @@
 #coding: utf-8
 
-# todo:这个文件还没有为3.0修改过
-
 
 import csv
 import json
@@ -11,57 +9,57 @@ import time
 
 import requests
 
+from yobot_errors import Server_error
 
-class Consult():
+
+class Consult:
     URL = "http://api.yobot.xyz/v2/nicknames/?type=csv"
     Feedback_URL = "http://api.yobot.xyz/v2/nicknames/?type=feedback&name="
 
-    def __init__(self):
-        path = os.path.dirname(sys.argv[0])
+    def __init__(self, glo_setting: dict):
+        self.setting = glo_setting
         self.nickname = {}
         self.number = {}
-        self.def_lst = []
-        nickfile = os.path.join(path, "nickname.csv")
-        self.txt_list = []
+        nickfile = os.path.join(glo_setting["dirname"], "nickname.csv")
         if not os.path.exists(nickfile):
             res = requests.get(self.URL)
-            assert res.status_code == 200, "服务器不可用"
+            if res.status_code != 200:
+                raise Server_error(
+                    "bad server response. code: "+str(res.status_code))
             with open(nickfile, "w", encoding="utf-8-sig") as f:
                 f.write(res.text)
-        with open(nickfile, encoding="utf-8-sig")as f:
+        with open(nickfile, encoding="utf-8-sig") as f:
             f_csv = csv.reader(f)
             for row in f_csv:
                 for col in row[1:]:
                     self.nickname[col] = row[0]
                 self.number[int(row[0])] = row[1]
 
-    def user_input(self, cmd):
+    def user_input(self, cmd: str) -> dict:
+        def_lst = []
         in_list = cmd.split()
         if len(in_list) > 5:
-            self.txt_list.append("防守人数过多")
-            return 5
+            return {"code": 5, "msg": "防守人数过多"}
         for index in in_list:
             item = self.nickname.get(index.lower(), "error")
             if item == "error":
                 requests.get(self.Feedback_URL+index)
-                self.txt_list.append("没有找到"+index+"，已发送反馈")
-                os.remove(os.path.join(os.path.dirname(
-                    sys.argv[0]), "nickname.csv"))
-                return 1
-            self.def_lst.append(item)
-        return 0
+                return {"code": 1, "msg": "没有找到"+index}
+            def_lst.append(item)
+        return {"code": 0, "def_lst": def_lst}
 
-    def jjcsearch(self):
-        query = ".".join(self.def_lst)
+    def jjcsearch(self, def_lst: list) -> str:
+        reply = ""
+        query = ".".join(def_lst)
         data = requests.get("http://api.yobot.xyz/jjc_search?def=" + query)
         res = json.loads(data.text)
         if(res["code"] == 0):
-            self.txt_list.append("从pcrdfans.com找到{}条记录".format(
-                len(res["data"]["result"])))
-            line = "======="
+            reply += "从pcrdfans.com找到{}条记录\n".format(
+                len(res["data"]["result"]))
+            line = "\n=======\n"
             for result in res["data"]["result"]:
-                self.txt_list.append(line)
-                line = "-------"
+                reply += line
+                line = "\n-------\n"
                 text = ""
                 for atker in result["atk"]:
                     text += self.number[atker["id"]]
@@ -73,11 +71,33 @@ class Consult():
                             cmt += "专"
                         text += "("+cmt+")"
                     text += " "
-                text += "({},{}赞{}踩)；".format(
+                text += "({},{}赞{}踩)".format(
                     result["updated"][2:10],
                     result["up"],
                     result["down"])
-                self.txt_list.append(text)
+                reply += text
         else:
-            text = "error code: {}, message : {}".format(
-                res["code"], res["message"])
+            raise Server_error("error code: {}, message : {}".format(
+                res["code"], res["message"]))
+        return reply
+
+    @staticmethod
+    def match(cmd: str) -> int:
+        if cmd.startswith("jjc查询"):
+            return 1
+        else:
+            return 0
+
+    def excute(self, match_num: int = 0, msg: dcit) -> dict:
+        if self.setting["jjc_consult"] == False:
+            reply = "此功能未启用"
+        else:
+            anlz = user_input(msg["raw_message"][5:])
+            if anlz["code"] == 0:
+                reply = self.jjcsearch(anlz["def_lst"])
+            else:
+                reply = anlz["msg"]
+        return {
+            "reply": reply
+            "block": True
+        }
--- src/client/plugins/yobot_errors.py
+++ src/client/plugins/yobot_errors.py
@@ -1,4 +1,8 @@
 # coding=utf-8
 class File_error(IOError):
     def __init__(self, s="file error"):
-        self.error_filename = s
+        self.error_msg = s
+
+class Server_error(Exception)
+    def __init__(self, s="server error"):
+        self.error_msg = s
