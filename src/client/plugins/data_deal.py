d582629e3a2332511b1f9acb02b500c1166f2e22
jiangwwzz222 <819211833@qq.com>
2019-12-07 20:34:19 +0800

    modified:   src/client/plugins/data_deal.py

--- src/client/plugins/data_deal.py
+++ src/client/plugins/data_deal.py
@@ -13,9 +13,7 @@ def init():
         json_deal.decodeFile(item)
 
     for item in os.listdir(contPath):
-        name = t2s.convert(item[:-4])
-        print(name)
-        search_data.store[name] = json_deal.decodejson(item)
+        search_data.store[item[:-4]] = json_deal.decodejson(item)
     util_data = search_data.store['unit_data']
     unit_promotion = search_data.store['unit_promotion']
     equipment_data = search_data.store['equipment_data']
@@ -24,7 +22,7 @@ def init():
     list3 = {}
     for data in util_data:
         if int(data['unit_id']) < 200000:
-            list[data['unit_name']] = data
+            list[t2s.convert(data['unit_name'])] = data
     search_data.store['unit_data'] = list
     for data in unit_promotion:
         if data['unit_id'] in list2:
