2804a05f37f9e77dc1739520f81645deb886b3b0
jiangwwzz222 <819211833@qq.com>
2019-12-07 19:16:10 +0800

    modified:   src/client/plugins/data_deal.py

--- src/client/plugins/data_deal.py
+++ src/client/plugins/data_deal.py
@@ -8,10 +8,12 @@ outPath = "./../cont"
 def init():
     for top, dirs, nondirs in os.walk(contPath):
         for item in nondirs:
+            print("json init" + item)
             json_deal.decodeFile(item)
 
     for top, dirs, nondirs in os.walk(outPath):
         for item in nondirs:
+            print("out init" + item)
             search_data.store[item[:-4]] = json_deal.decodejson(item)
     util_data = search_data.store['unit_data']
     unit_promotion = search_data.store['unit_promotion']
